29616838914d41dcb6ae337986fba844ac0c27c1
djcarrad <djcarrad@gmail.com>
2025-06-15 14:16:09 +0200

    make favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,9 @@ project = 'qcodes++'
 copyright = '2025, qcodes++'
 author = 'qcodes++'
 release = '0.1.0'
+version = '0.1.0'
+
+html_favicon = 'qcppfavicon.svg'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
